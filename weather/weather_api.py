0859473de71daf5da94aee5ba95ea117a0397904
Tema Shubin <shubin.artem@internet.ru>
2022-02-03 23:26:47 +0300

    Абстрактный класс и метод
    Метод возвращает погоду по его координатам

---
+++ weather/weather_api.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from .weather_data import WeatherInfo
+from storage.data_type import Coordinates
+
+
+class WeatherAPI(ABC):
+    @abstractmethod
+    def get_weather_data(self, coordinates: Coordinates) -> WeatherInfo:
+        pass
