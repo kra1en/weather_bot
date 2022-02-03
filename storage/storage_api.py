82e4df2cbe09a10884372fdd122162513230e332
Tema Shubin <shubin.artem@internet.ru>
2022-02-03 23:21:41 +0300

    Новый абстрактный метод
    Метод возвращает координаты города по его id

--- storage/storage_api.py
+++ storage/storage_api.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
+from .data_type import Settlement, Coordinates
 
 
 class StorageAPI(ABC):
     @abstractmethod
-    def get_settlements(self, settlement) -> []:
+    def get_settlements(self, settlement) -> list[Settlement]:
+        pass
+
+    @abstractmethod
+    def get_coordinates(self, settlement_id: int) -> Coordinates:
         pass
