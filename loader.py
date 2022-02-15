6551fbe217001de8089e8051c41321d32acfa399
Kirill Lapushinskiy <kirilllapushinskiy@yahoo.com>
2022-02-16 00:05:10 +0300

    Added command line args options.

--- loader.py
+++ loader.py
@@ -1,23 +1,38 @@
-from aiogram import Dispatcher
-from utils import WebhookModel, PollingModel
+from utils import WebhookModel, PollingModel, parse_config
+from storage import Storage
 import argparse
 
 __all__ = ['dp', 'engine', 'bot']
 
 parser = argparse.ArgumentParser("main.py")
 
-parser.add_argument("--polling", "-p",
-                    help="Run the application in polling mode.",
-                    action="store_true"
+parser.add_argument("--mode", "-m", nargs=1,
+                    help="Run the application in polling mode or webhook mode.",
+                    default='polling',
+                    choices=['polling', 'webhook']
                     )
 
-parser.add_argument("--webhook", "-w",
-                    help="Run the application in webhook mode.",
-                    action="store_true"
+parser.add_argument("--database", "-d", nargs=1,
+                    help="Run the application with sqlite or postgres.",
+                    default='sqlite',
+                    choices=['sqlite', 'postgres']
+                    )
+
+parser.add_argument("--in-memory", action='store_true',
+                    help="Run in-memory mode for sqlite.",
                     )
 
 args = parser.parse_args()
+cfg = parse_config('project.json')
 
-engine = WebhookModel() if args.webhook else PollingModel()
+storage = Storage(
+    database=Storage.SQLITE if args.database[0] == 'sqlite' else Storage.POSTGRESQL,
+    in_memory=args.in_memory,
+    source_file=cfg.src.file,
+    db_name=cfg.db.sqlite.file if args.database[0] == 'sqlite' else cfg.db.postgres.name,
+    db_port=cfg.db.postgres.port
+)
+
+engine = WebhookModel() if 'webhook' == args.mode else PollingModel()
 dp = engine.get_dispatcher()
 bot = engine.get_bot()
