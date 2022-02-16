5e48fb95b9b7486ae592deb1641e4e462797178b
Tema <im-unstoppable@mail.ru>
2022-02-16 14:08:16 +0300

    Добавлен словарь с временем суток

--- markups/dicts_weather.py
+++ markups/dicts_weather.py
@@ -1,2 +1,4 @@
+import datetime
 dict_wind = {"nw": "северо-западное", "n": "северное", "ne": "северо-восточное", "e": "восточное", "se": "юго-восточное", "s": "южное", "sw": "юго-западное", "w": "западное", "c": "штиль"}
 dict_cond = {"clear": "Ясно", "partly-cloudy": "Мало-облачно", "cloudy": "Облачно с прояснениями", "overcast": "Пасмурно", "drizzle": "Морось", "light-rain": "Небольшой дождь", "rain": "Дождь", "moderate-rain": "Умеренно усиленный дождь", "heavy-rain": "Сильный дождь", "continuous-heavy-rain": "Длительный сильный дождь", "showers": "Ливень", "wet-snow": "Дождь со снегом", "light-snow": "Небольшой снег", "snow": "Снег", "snow-showers": "Снегопад", "hail": "Град", "thunderstorm": "Гроза", "thunderstorm-with-rain": "Дождь с грозой", "thunderstorm-with-hail ": "Гроза с градом",}
+dict_part = {0: "Ночь", 1: "Утро", 2: "День", 3: "Вечер"}
