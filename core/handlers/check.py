02f2e9cc53eeae5894811fd1f5764a79700e8761
Tema <im-unstoppable@mail.ru>
2022-03-01 00:32:20 +0300

    Добавлено состояние

--- core/handlers/check.py
+++ core/handlers/check.py
@@ -2,10 +2,12 @@ from loader import dp, storage, weather
 
 from markups.inline_sellector import InlineSettlementSelector as iss
 from markups.inline_weather import InlineWeather as iw
+from core.states import WeatherStates
 
-@dp.callback_query_handler(iss.callback_data.filter())
+@dp.callback_query_handler(iss.callback_data.filter(), state = WeatherStates.search_settlement)
 async def check(callback_query, callback_data):
     if callback_data["action"] == '':
+        await WeatherStates.show_weather.set()
         coord = storage.get_coordinates(callback_data['settlement_id'])
         data = weather.get_weather_data(lat=coord.lat, lon=coord.lon)
         iw.initialization(days=data, user_id=callback_query.from_user.id)
