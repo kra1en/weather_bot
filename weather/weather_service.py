b1a1b3974c8ce7d9883a45b75aeae6051551eb39
Tema <im-unstoppable@mail.ru>
2022-03-01 00:23:20 +0300

    fix api

--- weather/weather_service.py
+++ weather/weather_service.py
@@ -6,7 +6,7 @@ from types import SimpleNamespace
 
 class WeatherService(WeatherAPI):
     def get_weather_data(self, lat: float, lon: float):
-        response = requests.get(f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&lang=ru_RU", headers={'X-Yandex-API-Key': 'b15dc6f6-9adc-430f-a304-ebbc0778b981'})
+        response = requests.get(f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&lang=ru_RU", headers={'X-Yandex-API-Key': 'f0621957-70cb-4ec5-809a-80ca80be06ff'})
         data = json.loads(response.text, object_hook=lambda d: SimpleNamespace(**d))
         result = []
         for num, day in enumerate(data.forecasts):
