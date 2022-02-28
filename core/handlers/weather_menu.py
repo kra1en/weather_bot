571abdc6af7d359df9d203de2b8713f9e0103116
Tema <im-unstoppable@mail.ru>
2022-03-01 00:28:43 +0300

    Добавили состояние

--- core/handlers/weather_menu.py
+++ core/handlers/weather_menu.py
@@ -1,7 +1,8 @@
 from loader import dp
 from markups.inline_weather import InlineWeather as iw
+from core.states import WeatherStates
 
-@dp.callback_query_handler(iw.callback_data.filter())
+@dp.callback_query_handler(iw.callback_data.filter(), state = WeatherStates.show_weather)
 async def weather_menu(callback_query, callback_data):
     await callback_query.message.edit_text(iw.update(user_id=callback_query.from_user.id, action=callback_data["action"]))
     await callback_query.message.edit_reply_markup(iw.get_markup(callback_query.from_user.id))
